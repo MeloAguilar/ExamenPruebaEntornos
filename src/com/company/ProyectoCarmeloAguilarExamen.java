12dbfbfd97bcdb776a9aadf4ac284257190663ee
MeloAguilar <carmelo.aguilar@iesnervion.es>
2020-12-11 12:04:02 +0100

    Holamundo

--- src/com/company/ProyectoCarmeloAguilarExamen.java
+++ src/com/company/ProyectoCarmeloAguilarExamen.java
@@ -3,6 +3,6 @@ package com.company;
 public class ProyectoCarmeloAguilarExamen {
 
     public static void main(String[] args) {
-        System.out.println("Aguilar Jimenez");
+        System.out.println("Hola Mundo");
     }
 }
